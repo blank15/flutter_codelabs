846fb14d05889f56af5f38a41c0f4fc4e91abea3
blank15 <yudistirosapturo@gmail.com>
2018-08-23 11:57:02 +0700

    Initial commit

---
+++ android/app/src/main/java/kampung/id/flutterfirst/MainActivity.java
@@ -0,0 +1,13 @@
+package kampung.id.flutterfirst;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
